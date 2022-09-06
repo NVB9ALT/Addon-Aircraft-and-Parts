136aad91fcd67298f0809169e60b8fb5751eaae1
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-09-05 19:53:56 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,152 @@
+var droptankF16 = "https://142420819-645052386429616373.preview.editmysite.com/uploads/1/4/2/4/142420819/370_gal_drop_tank.glb"
+var test1 = "https://142420819-645052386429616373.preview.editmysite.com/uploads/1/4/2/4/142420819/cockpitpanel__1_.glb"
+var su27airbrake = "https://142420819-645052386429616373.preview.editmysite.com/uploads/1/4/2/4/142420819/su-27_airbrake.glb"
+var condensationCones = ""
+var machCone = "https://142420819-645052386429616373.preview.editmysite.com/uploads/1/4/2/4/142420819/shockcone.glb"
+var parachute = "https://142420819-645052386429616373.preview.editmysite.com/uploads/1/4/2/4/142420819/parachute-proper.glb"
+var rainEffect = "https://geo-fs.com/models/precipitations/rain.gltf"
+geofs.debug.isSu27 = new Boolean(0)
+//see what M33.getOrientation(geofs.aircraft.instance.object3d._rotation) returns
+geofs.debug.createF16Tank = function() {
+   geofs.debug.F16Tank = {};
+	geofs.debug.F16Tank.model = new geofs.api.Model(droptankF16)
+}
+geofs.debug.loadF16Tank = function() {
+   geofs.debug.F16Tank || geofs.debug.createF16Tank()
+	try {
+        var c = V3.add(geofs.aircraft.instance.llaLocation, xyz2lla([0, 0, 0], geofs.aircraft.instance.llaLocation)),
+            d = M33.getOrientation(geofs.aircraft.instance.object3d._rotation);
+        geofs.debug.F16Tank.model.setPositionOrientationAndScale(c, d);
+    } catch (e) {
+	    throw("F16 tank loading error. " + e)
+    }
+};
+geofs.debug.createMachCone = function() {
+   geofs.debug.machCone = {};
+	geofs.debug.machCone.model = new geofs.api.Model(machCone)
+}
+geofs.debug.loadMachCone = function() {
+   geofs.debug.machCone || geofs.debug.createMachCone()
+	try {
+        var c = V3.add(geofs.aircraft.instance.llaLocation, xyz2lla([0, 0, 0], geofs.aircraft.instance.llaLocation)),
+            d = M33.getOrientation(geofs.aircraft.instance.object3d._rotation);
+        geofs.debug.machCone.model.setPositionOrientationAndScale(c, d);
+    } catch (e) {
+	    throw("Mach cone loading error. " + e)
+    }
+};
+geofs.debug.createParachute = function() {
+   geofs.debug.parachute = {};
+	geofs.debug.parachute.model = new geofs.api.Model(parachute)
+}
+geofs.debug.loadParachute = function() {
+   geofs.debug.parachute || geofs.debug.createParachute()
+	try {
+        var c = V3.add(geofs.aircraft.instance.llaLocation, xyz2lla([0, 0, 0], geofs.aircraft.instance.llaLocation)),
+            d = M33.getOrientation(geofs.aircraft.instance.object3d._rotation);
+        geofs.debug.parachute.model.setPositionOrientationAndScale(c, d);
+    } catch (e) {
+	    throw("Parachute loading error. " + e)
+    }
+};
+geofs.debug.createSu27Airbrake = function() {
+   geofs.debug.su27airbrake = {};
+	geofs.debug.su27airbrake.model = new geofs.api.Model(su27airbrake)
+}
+geofs.debug.loadSu27Airbrake = function() {
+   geofs.debug.su27airbrake || geofs.debug.createSu27Airbrake()
+	try {
+        var c = V3.add(geofs.aircraft.instance.llaLocation, xyz2lla([0, 0, 0], geofs.aircraft.instance.llaLocation)),
+            d = M33.getOrientation(geofs.aircraft.instance.object3d._rotation);
+        geofs.debug.su27airbrake.model.setPositionOrientationAndScale(c, d);
+    } catch (e) {
+	    throw("Airbrake loading error. " + e)
+    }
+};
+geofs.debug.update = function (a) {
+    geofs.debug.fps = exponentialSmoothing("fps", 1e3 / a).toPrecision(2);
+    if (geofs.debugOn) {
+        if ((a = $(".debugPointName")[0])) {
+            a = a.value;
+            var b = geofs.aircraft.instance.parts[a],
+                c = instruments.list[a];
+            if (b) {
+                var d = $(".debugCollisionPointIndex")[0].value;
+                d
+                    ? ((d = b.collisionPoints[parseInt(d)] || b.points[d]), geofs.debug.placeAxis(b.object3d.getWorldFrame(), d.worldPosition))
+                    : ($(".debugShowForceSource")[0].checked && geofs.debug.placeAxis(b.object3d.getWorldFrame(), b.points.forceSourcePoint.worldPosition),
+                      $(".debugShowForceDirection")[0].checked && geofs.debug.placeAxis(b.object3d.getWorldFrame(), b.points.forceDirection.worldPosition),
+                      $(".debugShowLocalPosition")[0].checked && geofs.debug.placeAxis(b.object3d.getWorldFrame(), b.object3d.worldPosition),
+                      $(".debugShowsuspensionOrigin")[0].checked && geofs.debug.placeAxis(b.object3d.getWorldFrame(), b.points.suspensionOrigin.worldPosition));
+                $(".debugPartData").html("Node Origin: " + b.object3d._nodeOrigin);
+            }
+            c && c.definition.cockpit && ((b = c.definition.cockpit.position), geofs.debug.placeAxis(geofs.aircraft.instance.object3d.getWorldFrame(), b.worldPosition));
+            "camera" == a && ((b = geofs.aircraft.instance.definition.camera.cockpit), geofs.aircraft.instance.object3d.setVectorWorldPosition(b), geofs.debug.placeAxis(geofs.aircraft.instance.object3d.getWorldFrame(), b.worldPosition));
+        }
+        geofs.debug.placingObjectId = $(".objectId").val();
+        geofs.debug.placingObjectId &&
+            $(".geofs-debugObjectLlaHtr").text(geofs.objects.getLla(geofs.debug.placingObjectId) + " " + geofs.objects.getHtr(geofs.debug.placingObjectId) + " " + geofs.objects.getScale(geofs.debug.placingObjectId));
+    }
+    //this works
+  if (geofs.animation.values.mach > 0.95 && geofs.animation.values.mach < 1.05) {
+	 geofs.debug.loadMachCone()
+  }
+  if (geofs.aircraft.instance.id == 7) {
+     geofs.debug.loadF16Tank()
+  }
+	 // brake parachute
+	 if (geofs.aircraft.instance.id == 7) { //compile database
+  if (geofs.animation.values.airbrakesTarget > 0 && geofs.animation.values.kias >= 10 && geofs.animation.values.kias <= 200) {
+geofs.debug.loadParachute()
+//increase drag a lot without having it increment (somehow)
+  }
+	 }
+  if (geofs.debug.isSu27 == 1 && geofs.animation.values.airbrakesTarget > 0) {
+    geofs.debug.loadSu27Airbrake()
+  }
+};
+
+
+//- Su-35 -> Su-27
+document.querySelectorAll('[data-aircraft]').forEach(function(e){
+   var elemName = e.outerText;
+    if (elemName.includes("Su-35")) {
+	    e.innerHTML = '<img src="images/planes/su35.png">Sukhoi Su-35<div data-aircraft="18" data-livery="0"><img src="images/planes/su35_0.png">Akula 35</div><div data-aircraft="18" data-livery="1"><img src="images/planes/su35_1.png">Sukhoi Su-27</div><div data-aircraft="18" data-livery="2"><img src="images/planes/su35_2.png">Russia Bort 06</div><div data-aircraft="18" data-livery="3"><img src="images/planes/su35_3.png">Russia Bort 901</div><div data-aircraft="18" data-livery="4"><img src="images/planes/su35_4.png">Ho Ho Ho</div>';
+    }
+});
+function runSu27() {
+if (geofs.aircraft.instance.id == 18 && geofs.aircraft.instance.liveryId == 1) {
+geofs.debug.isSu27 = 1
+geofs.aircraft.instance.definition.airbrakesTravelTime = 1
+geofs.aircraft.instance.definition.parts[46].animations[0].ratio = 0.069;
+geofs.aircraft.instance.definition.parts[46].animations[1].ratio = 0.069;
+geofs.aircraft.instance.definition.parts[51].animations[0].ratio = 0.069;
+geofs.aircraft.instance.definition.parts[51].animations[1].ratio = 0.069;
+/*geofs.aircraft.instance.setup.instruments = {
+        "cdi": "",
+        "compass": "",
+        "airspeedSupersonic": "",
+        "attitudeJet": "",
+        "altitude": "",
+        "varioJet": "",
+        "rpmJet": "",
+		"brakes": "",		
+		"gear": "",
+		"flaps": "",
+		"spoilers": ""
+}
+instruments.init(geofs.aircraft.instance.setup.instruments)*/
+if (geofs.animation.values.airbrakesTarget > 0) {
+   geofs.aircraft.instance.definition.dragFactor = 10
+} else {
+   geofs.aircraft.instance.definition.dragFactor = 1.7
+}
+   } else {
+geofs.debug.isSu27 = 0
+	}
+if (geofs.aircraft.instance.id == 18 && geofs.debug.isSu27 == 0 && geofs.aircraft.instance.definition.parts[46].animations[0].ratio == 0.069) {
+geofs.aircraft.instance.change(geofs.aircraft.instance.id, null, /*justReload*/ true, /*forceReset*/ false)
+ui.notification.show("Your spawn point has been reset to your current location (sorry)")
+   }
+};
+Su27Int = setInterval(function(){runSu27()},1000)
