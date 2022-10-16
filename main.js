e6c9e41504be1c98225eb58773ded682da99a3ae
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-10-16 11:50:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 geofs.addonAircraft = {};
-
-//-----F/A-18C Hornet-----------------------------------------------------------------------------------------------------
-//multiplayer visibility?
-//naming the thing
 document.querySelectorAll('[data-aircraft]').forEach(function(e){
    var elemName = e.outerText;
     if (elemName.includes("Su-35")) {
-       e.innerHTML = '<li data-aircraft="18"><img src="https://geo-fs.com/images/planes/su35.png">Sukhoi Su-35<div data-aircraft="18" data-livery="0"><img src="https://geo-fs.com/images/planes/su35_0.png">Akula 35</div><div data-aircraft="18" data-livery="1"><img src="https://geo-fs.com/images/planes/su35_1.png">Russia Bort 01</div><div data-aircraft="18" data-livery="2"><img src="https://geo-fs.com/images/planes/su35_2.png">Russia Bort 06</div><div data-aircraft="18" data-livery="3"><img src="https://geo-fs.com/images/planes/su35_3.png">Russia Bort 901</div></li>'
+	    e.innerHTML = '<img src="images/planes/su35.png">Sukhoi Su-35 Flanker-E<div data-aircraft="18" data-livery="0"><img src="images/planes/su35_0.png">Akula 35</div><div data-aircraft="18" data-livery="2"><img src="images/planes/su35_2.png">Russia Bort 06</div><div data-aircraft="18" data-livery="3"><img src="images/planes/su35_3.png">Russia Bort 901</div>';
     }
 });
+//-----F/A-18C Hornet-----------------------------------------------------------------------------------------------------
+//multiplayer visibility?
+//adding the button
 geofs.addonAircraft.runFA18 = function(){
    console.log("Loading F/A-18C")
    geofs.aircraft.instance.change(18, 4)
@@ -105,6 +104,7 @@ setTimeout(() => {
 },10000)
    } else {
 geofs.addonAircraft.isFA18 = 0
+geofs.f18instruments = 0
    }
 }
 checkRunHornetInterval = setInterval(function(){runHornet()},100)
@@ -114,4 +114,52 @@ checkRunHornetInterval = setInterval(function(){runHornet()},100)
 //Base aircraft: Alphajet
 
 //-----Su-27 Flanker (the OG one)-------------------------------------------------------------------------------------
-//Already made, just have to rework implementation
+geofs.addonAircraft.isSu27 = new Boolean(0)
+geofs.debug.su27Instruments = new Boolean(0)
+geofs.addonAircraft.runSu27 = function(){
+   geofs.aircraft.instance.change(18, 1)
+	geofs.addonAircraft.isSu27 = 1
+}
+flankerLi = document.createElement("li");
+flankerLi.innerHTML = '<div><img src="images/planes/su35_1.png">Sukhoi Su-27 Flanker</div>';
+flankerLi.addEventListener("click", geofs.addonAircraft.runSu27);
+document.getElementsByClassName("geofs-list geofs-toggle-panel geofs-aircraft-list")[0].appendChild(flankerLi)
+function runSu27() {
+if (geofs.aircraft.instance.id == 18 && geofs.aircraft.instance.liveryId == 1) {
+geofs.aircraft.instance.definition.airbrakesTravelTime = 1
+geofs.aircraft.instance.definition.parts[46].animations[0].ratio = 0.069;
+geofs.aircraft.instance.definition.parts[46].animations[1].ratio = 0.069;
+geofs.aircraft.instance.definition.parts[51].animations[0].ratio = 0.069;
+geofs.aircraft.instance.definition.parts[51].animations[1].ratio = 0.069;
+geofs.aircraft.instance.engines[0].thrust = 60000
+geofs.aircraft.instance.engines[0].afterBurnerThrust = 80000
+geofs.aircraft.instance.engines[1].thrust = 60000
+geofs.aircraft.instance.engines[1].afterBurnerThrust = 80000
+if (geofs.debug.su27Instruments == 0) {
+geofs.aircraft.instance.setup.instruments = {
+        "cdi": "",
+        "compass": "",
+        "airspeedSupersonic": "",
+        "attitudeJet": "",
+        "altitude": "",
+        "varioJet": "",
+        "rpmJet": "",
+		"brakes": "",		
+		"gear": "",
+		"flaps": "",
+		"spoilers": ""
+}
+instruments.init(geofs.aircraft.instance.setup.instruments)
+geofs.debug.su27Instruments = 1
+}
+if (geofs.animation.values.airbrakesTarget > 0) {
+   geofs.aircraft.instance.definition.dragFactor = 7.5
+} else {
+   geofs.aircraft.instance.definition.dragFactor = 0.5
+}
+   } else {
+geofs.debug.su27Instruments = 0
+geofs.addonAircraft.isSu27 = 0
+	}
+};
+Su27Int = setInterval(function(){runSu27()},100)
