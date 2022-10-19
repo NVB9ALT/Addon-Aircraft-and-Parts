7fd71d4cf65e280a6ddbf49c43411c75a835d190
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-10-19 09:08:48 -0700

    Add MiG-17

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ document.querySelectorAll('[data-aircraft]').forEach(function(e){
     }
 });
 //-----F/A-18C Hornet-----------------------------------------------------------------------------------------------------
-//multiplayer visibility?
 //adding the button
 geofs.addonAircraft.runFA18 = function(){
    console.log("Loading F/A-18C. Model credit to cs09736")
@@ -110,8 +109,41 @@ geofs.f18instruments = 0
 checkRunHornetInterval = setInterval(function(){runHornet()},100)
 
 //-----Mig-17 Fresco--------------------------------------------------------------------------------------------------
-//Cockpit model: Alphajet cockpit
-//Base aircraft: Alphajet
+geofs.addonAircraft.isMig17 = 0
+geofs.addonAircraft.runMiG17 = function(){
+   console.log("Loading MiG-17")
+}
+mig17Li = document.createElement("li");
+mig17Li.innerHTML = '<div><img src="https://finescale.com/~/media/images/workbench-reviews/2020/february-2020/fsmwb1219_zvezda_mig17_01.jpg">Mikoyan-Gurevich MiG-17 Fresco</div>';
+mig17Li.addEventListener("click", geofs.addonAircraft.runMiG17);
+//this works actually
+mig17Li.setAttribute("data-aircraft", 3)
+mig17Li.setAttribute("data-livery", 1)
+document.getElementsByClassName("geofs-list geofs-toggle-panel geofs-aircraft-list")[0].appendChild(mig17Li)
+function runMiG17() {
+   if (geofs.aircraft.instance.id == 3 && geofs.aircraft.instance.liveryId == 1) {
+geofs.aircraft.instance.definition.parts[3].area = 3
+geofs.aircraft.instance.definition.parts[4].area = 3
+geofs.aircraft.instance.definition.parts[8].liftFactor = 7
+geofs.aircraft.instance.definition.parts[9].liftFactor = 7
+geofs.aircraft.instance.definition.parts[8].dragFactor = 1
+geofs.aircraft.instance.definition.parts[9].dragFactor = 1
+geofs.aircraft.instance.definition.parts[16].liftFactor = 8
+geofs.aircraft.instance.engines[0].thrust = 15000
+geofs.aircraft.instance.engines[1].thrust = 15000
+geofs.aircraft.instance.engines[0].afterBurnerThrust = 20000
+geofs.aircraft.instance.engines[1].afterBurnerThrust = 20000
+geofs.aircraft.instance.cockpitSetup.parts[1].object3d.model._model.color.alpha = 0
+setTimeout(() => {
+   geofs.addonAircraft.isMig17 = 1
+},5000)
+setTimeout(() => {
+   geofs.aircraft.instance.definition.parts[0].animations[0].value = "rpm"
+	geofs.aircraft.instance.definition.parts[0].animations[0].gt = -1
+},10000)
+   }
+}
+mig17Int = setInterval(function(){runMiG17()},100)
 
 //-----Su-27 Flanker (the OG one)-------------------------------------------------------------------------------------
 geofs.addonAircraft.isSu27 = new Boolean(0)
