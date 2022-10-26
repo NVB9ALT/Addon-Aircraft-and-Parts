aac59e4519d5c25d59b9e3b857a846fb02038aef
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-10-26 09:13:40 -0700

    that compressor stall problem

--- F-14A.js
+++ F-14A.js
@@ -12,16 +12,26 @@ F14ALi.setAttribute("data-livery", 1)
 document.getElementsByClassName("geofs-list geofs-toggle-panel geofs-aircraft-list")[0].appendChild(F14ALi)
 function runF14A() {
 if (geofs.aircraft.instance.id == 2581 && geofs.aircraft.instance.liveryId == 1) {
-   if (geofs.animation.values.mach >= 1.75) {
+   if (geofs.animation.values.mach >= 1.75 && geofs.animation.values.slipball <= 1.2 && geofs.animation.values.slipball >= -1.2) {
 geofs.aircraft.instance.engines[0].thrust = 90000
 geofs.aircraft.instance.engines[0].afterBurnerThrust = 120000
 geofs.aircraft.instance.engines[1].thrust = 90000
 geofs.aircraft.instance.engines[1].afterBurnerThrust = 120000
-   } else {
+   } else if (geofs.animation.values.slipball <= 1.2 && geofs.animation.values.slipball >= -1.2) {
 geofs.aircraft.instance.engines[0].thrust = 60000
 geofs.aircraft.instance.engines[0].afterBurnerThrust = 90000
 geofs.aircraft.instance.engines[1].thrust = 60000
 geofs.aircraft.instance.engines[1].afterBurnerThrust = 90000
+	}
+	if (geofs.animation.values.kias > 50) {
+if (geofs.animation.values.slipball >= 1.2) { //right
+   geofs.aircraft.instance.engines[0].thrust = 5000
+	geofs.aircraft.instance.engines[0].afterBurnerThrust = 30000
+}
+if (geofs.animation.values.slipball <= -1.2) { //left
+   geofs.aircraft.instance.engines[1].thrust = 5000
+	geofs.aircraft.instance.engines[1].afterBurnerThrust = 30000
+}
 	}
 setTimeout(() => {
    geofs.addonAircraft.isF14A = 1
